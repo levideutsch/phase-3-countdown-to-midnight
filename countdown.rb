41f74ae556291cb5587b447439d3f9d9bee05b88
levideutsch <levideutsch@live.com>
2023-02-21 13:34:56 -0500

    commit tree 850ba72c00c5e90c42d1b4c7190706372fe9f907

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(number)
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number -= 1
+    end
+     "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num) 
+    while num > 0
+        num -=
+        sleep(5)
+    end
 end
